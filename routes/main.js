05ceb54e5ad9b23be3f50a68e183e0268030b099
Mackensie Jack <kensiejack1@gmail.com>
2023-05-20 12:00:56 -0400

    add comments

--- routes/main.js
+++ routes/main.js
@@ -5,7 +5,6 @@ const homeController = require("../controllers/home");
 const postsController = require("../controllers/posts");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
-//Main Routes - simplified for now
 router.get("/", homeController.getIndex);
 router.get("/profile", ensureAuth, postsController.getProfile);
 router.get("/feed", ensureAuth, postsController.getFeed);
