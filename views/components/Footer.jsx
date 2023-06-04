3ec789c3fa345264ea9dc8d16218f4f8a399b2ea
Mackensie Jack <kensiejack1@gmail.com>
2023-06-04 10:07:35 -0400

    arrow conversion

--- views/components/Footer.jsx
+++ views/components/Footer.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
-export default function Footer() {
-    return (
-    <>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>   
-    </>
-    )
-}
+const Footer = () => (
+  <>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
+      crossorigin="anonymous"
+    ></script>
+  </>
+);
+
+export default Footer;
--- views/components/Header.jsx
+++ views/components/Header.jsx
@@ -1,14 +1,14 @@
-import React from "react" 
+import React from "react";
 
-export default function Header(){
- return (
-   
-        <header className="text-stone-800">
-          <div className="text-center">
-            <h1 className="font-cherry lowercase text-4xl"><a href="/profile">Secondhand.</a></h1>
-            <span className="font-sans">Buy nothing, save the planet.</span>
-          </div>
-        </header>
-     
- )
-}
+const Header = () => (
+  <header className="text-stone-800">
+    <div className="text-center">
+      <h1 className="font-cherry lowercase text-4xl">
+        <a href="/profile">Secondhand.</a>
+      </h1>
+      <span className="font-sans">Buy nothing, save the planet.</span>
+    </div>
+  </header>
+);
+
+export default Header;
